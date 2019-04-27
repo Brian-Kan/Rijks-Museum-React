dd9d3346a84b3c29aacfa9e7d402faad540d34e9
Brian Kan <BrianKan00@hotmail.com>
2019-04-27 14:41:29 -0400

    Updates

---
+++ precache-manifest.0da125b4789b4f069fd17d5333884ff4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c91be860587221b9ede8e5e45906ed63",
+    "url": "/Rijks-Museum-React/index.html"
+  },
+  {
+    "revision": "4839b95e707fbef56ab8",
+    "url": "/Rijks-Museum-React/static/css/main.8fe96998.chunk.css"
+  },
+  {
+    "revision": "496d96f4d48f7360a369",
+    "url": "/Rijks-Museum-React/static/js/2.50f5a3e5.chunk.js"
+  },
+  {
+    "revision": "4839b95e707fbef56ab8",
+    "url": "/Rijks-Museum-React/static/js/main.b07da99c.chunk.js"
+  },
+  {
+    "revision": "57201b9fc06b77b5b31e",
+    "url": "/Rijks-Museum-React/static/js/runtime~main.c591e938.js"
+  }
+]);
